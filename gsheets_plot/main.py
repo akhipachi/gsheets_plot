a417ce86996ae141be77d495c7bc97c6c725b054
akhilesh <akhilesh.pachipulusu@inviz.ai>
2020-10-04 23:43:51 +0530

    added gey by url

--- gsheets_plot/main.py
+++ gsheets_plot/main.py
@@ -9,28 +9,31 @@ class Gsheets:
     def get_titles(self,unique:bool=False):
         return self.gsheets.titles(unique=unique)
     
-    def get_sheet(self,title:str):
+    def get_sheet_title(self,title:str):
         return self.gsheets.find(title=title)
 
-    def get_dataframe(self,title:str,sheet:str='Sheet1'):
-        book=self.get_sheet(title=title)
+    def get_sheet_url(self,url:str):
+        return self.gsheets.get(url)
+
+    def get_dataframe(self,title:str=None,url:str=None,sheet:str='Sheet1'):
+        if title:
+            book=self.get_sheet_title(title=title)
+        if url:
+            book=self.get_sheet_url(url=url)
         sheet=book.find(sheet)
         return sheet.to_frame()
 
 class Plot:
-    def __init__(self):
+    def __init__(self,title:str=None,url:str=None,sheet:str='Sheet1'):
         self.gsheets=Gsheets()
-    
-    def get_dataframe(self,title:str,sheet:str='Sheet1'):
-        return self.gsheets.get_dataframe(title=title,sheet=sheet)
+        self.df=self.gsheets.get_dataframe(title=title,url=url,sheet=sheet)
+        
 
-    def get_columns(self,title:str,sheet:str='Sheet1'):
-        df=self.get_dataframe(title=title,sheet=sheet)
-        return df.columns
+    def get_columns(self):
+        return self.df.columns
 
-    def plot(self,x:str,y:str,title:str,sheet:str='Sheet1'):
-        df=self.get_dataframe(title=title,sheet=sheet)
-        plt.plot(df[x],df[y])
+    def plot(self,x:str,y:str):
+        plt.plot(self.df[x],self.df[y])
         plt.xlabel(x)
         plt.ylabel(y)
         plt.savefig('plot.png')
